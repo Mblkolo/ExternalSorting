9f330073b382f1be42067e7c021d5bb9d8429f2d
Mblkolo <rusakov-rv@yandex.ru>
2023-11-19 16:51:31 +0500

    Добавлены тесты на соритровку

--- ExternalSorting.Tests/OuterSortingTests.cs
+++ ExternalSorting.Tests/OuterSortingTests.cs
@@ -2,26 +2,65 @@ namespace ExternalSorting.Tests;
 
 public class OuterSortingTests
 {
-    [Fact]
-    public void DirectOuterSortTest()
-    {
-        var sort = new DirectOuterSort(1);
+    private readonly string SampleFilePath = Path.Combine("Samples", "A.csv");
+    private readonly string SortingFilePath = "A.csv";
+    private static string ExpectedFilePath(string fileName) => Path.Combine("Samples", fileName);
 
+
+    [Theory]
+    [InlineData(0, "Sort0.csv")]
+    [InlineData(1, "Sort1.csv")]
+    [InlineData(2, "Sort2.csv")]
+    public void DirectOuterSortTest(int fieldNo, string expectedFileName)
+    {
+        File.Copy(SampleFilePath, SortingFilePath, true);
+        var sort = new DirectOuterSort(fieldNo);
+
+        //Act
         sort.Sort();
+
+        //Assert
+        var sortedLines = File.ReadAllLines(SortingFilePath);
+        var expectedLines = File.ReadAllLines(ExpectedFilePath(expectedFileName));
+
+        Assert.Equal(expectedLines, sortedLines);
     }
-    [Fact]
-    public void NaturalOuterSortTest()
-    {
-        var sort = new DirectOuterSort(1);
 
+    [Theory]
+    [InlineData(0, "Sort0.csv")]
+    [InlineData(1, "Sort1.csv")]
+    [InlineData(2, "Sort2.csv")]
+    public void NaturalOuterSortTest(int fieldNo, string expectedFileName)
+    {
+        File.Copy(SampleFilePath, SortingFilePath, true);
+        var sort = new DirectOuterSort(fieldNo);
+
+        //Act
         sort.Sort();
+
+        //Assert
+        var sortedLines = File.ReadAllLines(SortingFilePath);
+        var expectedLines = File.ReadAllLines(ExpectedFilePath(expectedFileName));
+
+        Assert.Equal(expectedLines, sortedLines);
     }
 
-    [Fact]
-    public void MultiThreadOuterSortTest()
+    [Theory]
+    [InlineData(0, "Sort0.csv")]
+    [InlineData(1, "Sort1.csv")]
+    [InlineData(2, "Sort2.csv")]
+    public void MultiThreadOuterSortTest(int fieldNo, string expectedFileName)
     {
-        var sort = new MultiThreadOuterSort(1);
+        File.Copy(SampleFilePath, SortingFilePath, true);
+        var sort = new MultiThreadOuterSort(fieldNo);
 
+        //Act
         sort.Sort();
+
+        //Assert
+        var sortedLines = File.ReadAllLines(SortingFilePath);
+        var expectedLines = File.ReadAllLines(ExpectedFilePath(expectedFileName));
+
+        Assert.Equal(expectedLines, sortedLines);
     }
 }
